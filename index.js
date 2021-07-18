7be2ceca7bc3914168ef554367ed4a88f4730748
SauceArgente <silversocce@gmail.com>
2021-07-18 16:59:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const Render = require("./lib/Render");
+const Component = require("./lib/Component");
+const Element = require("./lib/Element");
+
+module.exports = {
+    Render,
+    Component,
+    Element,
+    el: Element,
+}

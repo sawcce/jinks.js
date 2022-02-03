9da241a36ebeeb77cce02804a1441dd0168e21cd
SauceArgente <alexis.collieux@gmail.com>
2022-02-03 22:21:43 +0100

    Fixed ghost import for deprecated render

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 ///<reference path="./index.d.ts"/>
 
-import _Render from './lib/Render';
 import _Component from './lib/Component';
 import _Element from './lib/Element';
 
-export let Render = _Render
 export let Component = _Component
-export let Element = _Element
-export let el = Element
+export let el = _Element
